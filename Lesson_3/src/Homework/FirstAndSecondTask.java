80afad7b6a5b40691fa213184efa78b358d87fc0
Roman <rzozulya1997@gmail.com>
2023-04-24 11:49:56 +0300

    add prectical task

--- Lesson_3/src/Homework/FirstAndSecondTask.java
+++ Lesson_3/src/Homework/FirstAndSecondTask.java
@@ -4,49 +4,51 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class FirstAndSecondTask {
-        public static void main(String[] args) {
-            Scanner scanner =new Scanner(System.in);
-            while(true) {
-                Scanner sc = new Scanner(System.in);
-                System.out.println("Choose action:");
-                System.out.println("\t1 - Check first home task task ");
-                System.out.println("\t2 - Check second home task task");
-                System.out.println("\t3 - End work ");
-                int choice = sc.nextInt();
-                switch (choice) {
-                    case 1:
-                        firstHometask(scanner);
-                        break;
-                    case 2:
-                        secondHometask(scanner);
-                        break;
-                    case 3:
-                        sc.close();
-                        scanner.close();
-                        return;
-                    default:
-                        System.out.println("Necessary to enter option 1-3\n");
-                }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Choose action:");
+            System.out.println("\t1 - Check first home task task ");
+            System.out.println("\t2 - Check second home task task");
+            System.out.println("\t3 - End work ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    firstHometask(scanner);
+                    break;
+                case 2:
+                    secondHometask(scanner);
+                    break;
+                case 3:
+                    sc.close();
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Necessary to enter option 1-3\n");
             }
         }
-        public static void firstHometask(Scanner scanner){
-            double a,b,result;
-            System.out.print("Input Side 1:");
-            a=scanner.nextDouble();
-            System.out.print("Input Side 2:");
-            b=scanner.nextDouble();
-            result=0.5*a*b;
-            System.out.println("the area of the triangle is "+ result);
-        }
-        public static void secondHometask(Scanner scanner){
-            double[] minValue=new double[3];
-            System.out.print("Input the first number:");
-            minValue[0]=scanner.nextDouble();
-            System.out.print("Input the second number:");
-            minValue[1]=scanner.nextDouble();
-            System.out.print("Input the third number:");
-            minValue[2]=scanner.nextDouble();
-            Arrays.sort(minValue);
-            System.out.println("Min Value = " +minValue[0]);
-        }
+    }
+
+    public static void firstHometask(Scanner scanner) {
+        double a, b, result;
+        System.out.print("Input Side 1:");
+        a = scanner.nextDouble();
+        System.out.print("Input Side 2:");
+        b = scanner.nextDouble();
+        result = 0.5 * a * b;
+        System.out.println("the area of the triangle is " + result);
+    }
+
+    public static void secondHometask(Scanner scanner) {
+        double[] minValue = new double[3];
+        System.out.print("Input the first number:");
+        minValue[0] = scanner.nextDouble();
+        System.out.print("Input the second number:");
+        minValue[1] = scanner.nextDouble();
+        System.out.print("Input the third number:");
+        minValue[2] = scanner.nextDouble();
+        Arrays.sort(minValue);
+        System.out.println("Min Value = " + minValue[0]);
+    }
 }
--- Lesson_3/src/Practical/Main.java
+++ Lesson_3/src/Practical/Main.java
@@ -1,8 +1,20 @@
 package Practical;
 
+import Practical.Employee.Employee;
+
 public class Main{
     public static void main(String[] args) {
+        Employee e1 = new Employee("Roman",20,8);
+        Employee e2 = new Employee("Iryna",22,8);
+        Employee e3 = new Employee("Ivan",20,10);
 
+        System.out.println(e1);
+        System.out.println(e2);
+        System.out.println(e3);
+
+        System.out.println("The salary fisrt employee is "+e1.getSalary());
+        System.out.println("The salary second employee is "+e2.getBonus());
+        System.out.println("The salary third employee is "+e3.getSalary());
     }
 }
 
