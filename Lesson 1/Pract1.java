b2d2ccbfa75379c0a3c4160e28fb66988ab40d04
dmytro.lisovych <dmytro.lisovych@gmail.com>
2023-04-26 02:22:15 +0300

    Update Pract1.java

---
+++ Lesson 1/Pract1.java
@@ -0,0 +1,19 @@
+package com.lesson1;
+
+import java.util.Scanner;
+
+public class pract1 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first word: ");
+        String w1 = sc.nextLine();
+        System.out.println("Enter second word: ");
+        String w2 = sc.nextLine();
+        System.out.println("Enter third word: ");
+        String w3 = sc.nextLine();
+        System.out.println("\n" + w3 + "\n" + w2 + "\n" + w1);
+
+    }
+
+}
