2de31a371410570d7f465308f175a635fef6cd38
Roman <rzozulya1997@gmail.com>
2023-04-27 22:29:38 +0300

    add task from lesson 4

---
+++ Lesson_4/src/homeworks/Homework.java
@@ -0,0 +1,83 @@
+package homeworks;
+
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        homeWorkFirst(scanner);
+        homeWorkSecond();
+        homeWorkThird();
+
+    }
+
+    public static void homeWorkFirst(Scanner scanner) {
+
+        System.out.println("enter number:");
+        float a = scanner.nextFloat();
+        System.out.println("enter number:");
+        float b = scanner.nextFloat();
+        System.out.println("enter number:");
+        float c = scanner.nextFloat();
+        if (5 >= a && a >= -5 && 5 >= b && b >= -5 && 5 >= c && c >= -5) {
+            System.out.println("number is in range[-5,5]");
+        } else {
+            System.out.println("number isn't in range[-5,5]");
+        }
+        if (5 >= b && b >= -5) {
+            System.out.println("number is in range[-5,5]");
+        } else {
+            System.out.println("number isn't in range[-5,5]");
+        }
+        if (5 >= c && c >= -5) {
+            System.out.println("number is in range[-5,5]");
+        } else {
+            System.out.println("number isn't in range[-5,5]");
+        }
+
+    }
+
+    public static void homeWorkSecond() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number:");
+        int a = scanner.nextInt();
+        System.out.println("enter number:");
+        int b = scanner.nextInt();
+        System.out.println("enter number:");
+        int c = scanner.nextInt();
+        int max = a;
+        int min = a;
+        if (b > max) {
+            max = b;
+        } else if (max < c) {
+            max = c;
+        }
+        if (min > b) {
+            min = b;
+        } else if (min > c) {
+            min = c;
+        }
+        System.out.println("max number:" + max + "\nmin number:" + min);
+        scanner.close();
+    }
+
+    public static void homeWorkThird() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number error");
+        int whatTheError = scanner.nextInt();
+        if (whatTheError == 100 || whatTheError == 101 || whatTheError == 102 || whatTheError == 103) {
+            System.out.println(WhatTheError.INFO);
+        }
+        if (whatTheError == 200 || whatTheError == 201 || whatTheError == 202) {
+            System.out.println(WhatTheError.SUCCESS);
+        }
+        if (whatTheError == 300 || whatTheError == 301 || whatTheError == 302) {
+            System.out.println(WhatTheError.REDIRECTION);
+        }
+        if (whatTheError == 400 || whatTheError == 401 || whatTheError == 402) {
+            System.out.println(WhatTheError.CLIENTERROR);
+        }
+    }
+
+
+}
---
+++ Lesson_4/src/practical/PracticalTask.java
@@ -0,0 +1,149 @@
+package practical;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class PracticalTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Choose action:");
+            System.out.println("\t1 - Check first practical task task ");
+            System.out.println("\t2 - Check second practical task task");
+            System.out.println("\t3 - Check third practical task task");
+            System.out.println("\t4 - End work ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    firstPracticalTasks(scanner);
+
+                    break;
+                case 2:
+                    secondPracticalTasks(scanner);
+                    break;
+                case 3:
+                    thirdPracticalTasks(scanner);
+                    break;
+                case 4:
+                    sc.close();
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Necessary to enter option 1-4\n");
+            }
+        }
+
+    }
+
+    public static void firstPracticalTasks(Scanner scanner) {
+        int[] number = new int[6];
+        int odd = 0;
+        System.out.println("Enter number:");
+        for (int i = 0; i < 6; i++) {
+            number[i] = scanner.nextInt();
+        }
+        System.out.println("Our number: ");
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
+            if (number[i] % 2 != 0) {
+                odd++;
+            }
+        }
+        if (odd > 0) {
+            System.out.println("There are odd number: " + odd);
+        } else {
+            System.out.println("There aren't odd numbers");
+        }
+    }
+
+    public static void secondPracticalTasks(Scanner scanner) {
+        System.out.println("enter number of day");
+        int dayOfWeek = scanner.nextInt();
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Necessary to enter option 1-7\n");
+        }
+    }
+
+    public static void thirdPracticalTasks(Scanner scanner) {
+        enum Continent{
+            AFRICA,
+            ANTARCTICA,
+            ASIA,
+            AUSTRALIA,
+            EUROPE,
+            NORTH_AMERICA,
+            SOUTH_AMERICA
+        }
+        System.out.println("Enter the name of the country:");
+        String countryName= scanner.nextLine().toUpperCase();
+        Continent continent;
+        switch (countryName) {
+            case "USA":
+            case "CANADA":
+            case "MEXICO":
+            case "PANAMA":
+                continent = Continent.NORTH_AMERICA;
+                break;
+            case "ARGENTINA":
+            case "BRAZIL":
+            case "COLOMBIA":
+            case "PERU":
+                continent = Continent.SOUTH_AMERICA;
+                break;
+            case "ALGERIA":
+            case "EGYPT":
+            case "NIGERIA":
+            case "SOUTH AFRICA":
+                continent = Continent.AFRICA;
+                break;
+            case "CHINA":
+            case "INDIA":
+            case "JAPAN":
+            case "RUSSIA":
+                continent = Continent.ASIA;
+                break;
+            case "AUSTRALIA":
+            case "NEW ZEALAND":
+                continent = Continent.AUSTRALIA;
+                break;
+            case "FRANCE":
+            case "GERMANY":
+            case "ITALY":
+            case "SPAIN":
+                continent = Continent.EUROPE;
+                break;
+            case "ANTARCTICA":
+                continent = Continent.ANTARCTICA;
+                break;
+            default:
+                System.out.println("Sorry, we don't have information about this country's continent.");
+                return;
+        }
+        System.out.println("The continent of " + countryName + " is " + continent);
+
+    }
+}
