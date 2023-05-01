3f01f2f9bd24d69988c48963327296914ed82007
Maryna <m51571750@gmail.com>
2023-05-01 21:53:07 +0300

    Added homework 05

---
+++ test/homework/homework05/task1/FindTest.java
@@ -0,0 +1,35 @@
+package homework.homework05.task1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void positionPositive() {
+        int[] arr = {5, -1, -1, 3, 8};
+        assertEquals(3, Find.positionPositive(arr));
+    }
+    @Test
+    void positionPositiveNone() {
+        int[] arr = {5, -1, -1, -3, -8};
+        assertEquals(0, Find.positionPositive(arr));
+    }
+
+    @Test
+    void productValue() {
+        int[] arr = {5, -1, 0, 6, 9};
+        assertEquals(-270, Find.productValue(arr));
+    }
+}
