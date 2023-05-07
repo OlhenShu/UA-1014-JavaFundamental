199e99c43afa04e96b0f72eaf9f667e8274f8e54
Maryna <m51571750@gmail.com>
2023-05-07 22:31:23 +0300

    Added homework 05

--- src/homework/homework05/task2/Sum.java
+++ src/homework/homework05/task2/Sum.java
@@ -5,17 +5,13 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         do {
-            System.out.println(sumAndAnswer(sc));
+            System.out.print("Please, enter the 2 integer numbers: ");
+            System.out.println("Sum of this number is " + (sc.nextInt() + sc.nextInt()) + ".");
+            System.out.println("Do you want try again? (yes/no)");
+            sc.nextLine();
         }while(sc.nextLine().toLowerCase().equals("yes"));
 
         sc.close();
-    }
-    public static String sumAndAnswer(Scanner sc){
-        System.out.print("Please, enter the 2 integer numbers: ");
-        System.out.println("Sum of this number is " + (sc.nextInt() + sc.nextInt()) + ".");
-        System.out.println("Do you want try again? (yes/no)");
-        return sc.nextLine();
     }
 }
