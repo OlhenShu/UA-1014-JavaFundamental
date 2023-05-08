c97e071cf5b601ad5314c2b97d7b7b76a4b7ddba
Augustin Gerasym <augustingerasym@gmail.com>
2023-05-08 12:57:06 +0300

    add task lines

---
+++ src/practical-java/lessonSix/lines/ColorLine.java
@@ -0,0 +1,19 @@
+package lessonSix.lines;
+
+public class ColorLine extends Line {
+    Color color;
+
+    public ColorLine(Point starPoint, Point endPoint, Color color) {
+        super(starPoint, endPoint);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " in " + getColor() + " color";
+    }
+}
