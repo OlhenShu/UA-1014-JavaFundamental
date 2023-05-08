12706607589f37b4982df8a27d2c8f34af7dd81a
OlhenShu <olya.shutileva@gmail.com>
2023-05-08 16:00:25 +0300

    improve

--- src/homework/homework03/task1/Area.java
+++ src/homework/homework03/task1/Area.java
@@ -1,7 +1,7 @@
-
 package homework.homework03.task1;
 
 import java.util.Scanner;
+
 public class Area {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
@@ -21,3 +21,22 @@ public class Area {
         return Math.sqrt(p*(p-a)*(p-b)*(p-c));
     }
 }
+//public class Area {
+//    private static Scanner scanner = new Scanner(System.in);
+//    public static void main(String[] args){
+//
+//        int a = getNumber("Input Side 1: ");
+//        int b = getNumber("Input Side 2: ");
+//        int c = getNumber("Input Side 3: ");
+//
+//        System.out.printf("The area of the triangle is %.3f.",getArea(a, b, c));
+//    }
+//    static int getNumber(String prompt){
+//        System.out.print(prompt);
+//        return scanner.nextInt();
+//    }
+//    static double getArea(double a, double b, double c){
+//        double p = (a + b + c) / 2;
+//        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+//    }
+//}
