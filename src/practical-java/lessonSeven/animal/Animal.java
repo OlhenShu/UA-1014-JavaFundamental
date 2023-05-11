0a4e0c828b4fca0902a8fd6c628a547098cfb8cf
Augustin Gerasym <augustingerasym@gmail.com>
2023-05-11 14:52:13 +0300

    add task Animal

---
+++ src/practical-java/lessonSeven/animal/Animal.java
@@ -0,0 +1,10 @@
+package lessonSeven.animal;
+
+public interface Animal {
+    
+    void voice();
+
+    void feed();
+}
+
+
---
+++ src/practical-java/lessonSeven/animal/Dog.java
@@ -0,0 +1,12 @@
+package lessonSeven.animal;
+
+public class Dog implements Animal {
+
+    public void voice() {
+        System.out.println("Woof!");
+    }
+
+    public void feed() {
+        System.out.println("Feeding the dog");
+    }
+}
