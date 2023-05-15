914cf20378e818ed535cc212abb1a8c21d9843e3
OlhenShu <olya.shutileva@gmail.com>
2023-05-15 15:58:16 +0300

    improve

--- HomeworkJava/src/homework5/Task2.java
+++ HomeworkJava/src/homework5/Task2.java
@@ -3,9 +3,8 @@ package homework5;
 import java.util.Scanner;
 
 public class Task2 {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
         int a;
         int b;
         int c;
@@ -20,6 +19,23 @@ public class Task2 {
             c = sc.nextInt();
 
         }while (c < 2);
-
     }
+//    public static void main(String[] args) {
+//        int sum;
+//        char repeat;
+//
+//        do {
+//            System.out.print("Enter the first number: ");
+//            int num1 = scanner.nextInt();
+//
+//            System.out.print("Enter the second number: ");
+//            int num2 = scanner.nextInt();
+//
+//            sum = num1 + num2;
+//            System.out.println("Sum: " + sum);
+//
+//            System.out.print("Do you want to perform the operation again? (Y/N): ");
+//            repeat = scanner.next().charAt(0);
+//        } while (repeat == 'Y' || repeat == 'y');
+//    }
 }
