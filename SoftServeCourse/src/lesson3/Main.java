7bd0ecfa47483df88b8de316c30afd50eb5e6804
Vasyl <vacylhuk3@gmail.com>
2023-05-19 19:48:05 +0300

    task 4

---
+++ SoftServeCourse/src/lesson3/Main.java
@@ -0,0 +1,22 @@
+class  Scratch extends Object {
+    public  static void main(String[] args)
+        System.out.println(new A ());
+        System.out.println(new A ());
+        System.out.println(new A ());
+      }
+}
+
+class A { 3 usages
+    String name;  no usages
+            int age; no usages
+
+      public A() {
+      }
+
+    public A(String name) {
+                System.out.println("Hello");
+        this.name = name;
+    }
+
+
+}
