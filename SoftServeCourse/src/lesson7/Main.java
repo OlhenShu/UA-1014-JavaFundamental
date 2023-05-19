4968fec25bae56ec43db81d639199083ea231558
Vasyl <vacylhuk3@gmail.com>
2023-05-19 19:49:45 +0300

    task 4

---
+++ SoftServeCourse/src/lesson7/Main.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+interface Collection {
+    int DAYS_IN_WEEK = getDays; // screaming snake case
+
+    private static int getDays() {
+        return 7;
+    }
+    int size();
+    default boolean isEmpty(); {
+        foo1();
+        return size() == 0;;
+    }
+    private void foo3() {
+        foo1();
+        this.size();
+    }
+public static void () {
+    foo1();
+    foo2();
+}
+
+private static void foo1() {}
+private static void foo2() {}
+}
+
+interface I {
+    int DAYS_IN_WEEK = 7; // screaming snake case
+
+    public static void foo() {
+        foo1();
+        foo2();
+    }
+}
