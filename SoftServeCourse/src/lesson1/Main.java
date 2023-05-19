4fd0b61dd7e3b0a71187cd7657fa4f892f2140eb
Vasyl <vacylhuk3@gmail.com>
2023-05-19 19:47:28 +0300

    task 4

---
+++ SoftServeCourse/src/lesson1/Main.java
@@ -0,0 +1,10 @@
+public class Main1 {
+    public static void main1(String[] args) {
+        int a = 1 ;
+        System.out.println(a);
+        double b = a ;
+        System.out.println(b);
+        char c = 'A' ;
+        System.out.println((char)(c+ 1));
+    }
+}
