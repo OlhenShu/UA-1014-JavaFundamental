2fa101a60541863b472904686bf62c566d590be4
Augustin Gerasym <augustingerasym@gmail.com>
2023-05-21 00:48:08 +0300

    finish with task myCollection

--- src/main/java/homeworks/homeworkNine/myCollection/MyCollection.java
+++ src/main/java/homeworks/homeworkNine/myCollection/MyCollection.java
@@ -3,11 +3,13 @@ package homeworks.homeworkNine.myCollection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Collections;
 
 public class MyCollection {
     public static void main(String[] args) {
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
         List<Integer> myCollection = new ArrayList<>();
 
         int num = 10;
@@ -19,22 +21,34 @@ public class MyCollection {
         System.out.println();
 
         swap(myCollection);
-        System.out.println(myCollection);
+        System.out.println("Swap min and max elements: " + myCollection);
         System.out.println();
 
         randomThreeDigitNumber(myCollection, random);
-        System.out.println(myCollection);
+        System.out.println("Adding random three-digit number: " +myCollection);
         System.out.println();
 
         insertZero(myCollection);
-        System.out.println(myCollection);
+        System.out.println("Insert zero between all neighboring elements: " + myCollection);
         System.out.println();
 
-        int k = myCollection.size();
+        int k = inputValue(scanner);
 
         List<Integer> list1 = new ArrayList<>(myCollection.subList(0, k));
         Collections.sort(list1);
-        System.out.println(list1);
+        System.out.println("\nFirst 'k' elements sorted: " + list1);
+        System.out.println();
+
+        List<Integer> list2 = new ArrayList<>(myCollection.subList(k, myCollection.size()));
+        Collections.sort(list2);
+        Collections.reverse(list2);
+        System.out.println("Rest of elements sorted reverse: " + list2);
+        System.out.println();
+
+        deleteLastEvenNumber(myCollection);
+        System.out.println();
+
+        deleteNextAfterMinimum(myCollection);
     }
 
     public static void swap(List<Integer> collection) {
@@ -79,6 +93,50 @@ public class MyCollection {
                 collection.add(i + 1, 0);
             }
             i++;
+        }
+    }
+
+    public static int inputValue(Scanner scanner) {
+        System.out.print("Enter an integer value 5-10 for 'k': ");
+        int resilt = scanner.nextInt();
+        while (resilt < 5 || resilt > 10) {
+            System.out.println("Out of range. Expected 5-10");
+            System.out.print("Enter: ");
+            resilt = scanner.nextInt();
+        }
+        return resilt;
+    }
+
+    public static void deleteLastEvenNumber(List<Integer> collection) {
+        int evenNumber = 0;
+        int index = collection.get(0);
+        for (int i = collection.size() - 1; i >= 0; i--) {
+            if (collection.get(i) != 0 && collection.get(i) % 2 == 0) {
+                index = i;
+                evenNumber = collection.get(i);
+                break;
+            }
+        }
+        if (evenNumber != 0) {
+            collection.remove(index);
+            System.out.println("Remove last even number: " + collection);
+        } else System.out.println("There is no even numbers");
+    }
+
+    public static void deleteNextAfterMinimum(List<Integer> collection) {
+        int min = collection.get(0);
+        int indexOfMin = 0;
+        int count = 0;
+        for (var el : collection) {
+            if (el < min) {
+                min = el;
+                indexOfMin = count;
+            };
+            count++;
+        }
+        if (indexOfMin != collection.size() - 1) {
+            collection.remove(indexOfMin + 1);
+            System.out.println("Remove next after minimum element: " + collection);
         }
     }
 }
