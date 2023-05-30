30e9b094f633008807ba33ab1de73e2e5f0e0c2f
Maryna <m51571750@gmail.com>
2023-05-30 21:05:10 +0300

    Added changes

--- src/homework/homework13/task1/EncryptDecrypt.java
+++ src/homework/homework13/task1/EncryptDecrypt.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class EncryptDecrypt {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
-        System.out.print("Enter some word: ");
-        int someInt = 5;
-        String newWord = encrypt(sc.nextLine(), someInt);
-        System.out.printf("Encrypted word: %s. %n",newWord);
-        System.out.printf("Decoded word: %s.", decrypt(newWord, someInt));
+        try {
+            System.out.print("Enter some word: ");
+            String word = sc.nextLine();
+            System.out.print("Enter some integer number: ");
+            int someInt = sc.nextInt();
+            String newWord = encrypt(word, someInt);
+            System.out.printf("Encrypted word: %s. %n",newWord);
+            System.out.printf("Decoded word: %s.", decrypt(newWord, someInt));
+        }catch (Throwable t){
+            System.out.println("Something went wrong.");
+        }
+
     }
     private static String encrypt(String s, int n){
         String newString = "";
@@ -22,13 +29,6 @@ public class EncryptDecrypt {
         return newString;
     }
     private static String decrypt(String s, int n){
-        String newString = "";
-        for (int i = 0; i<s.length(); i++){
-            char letter = s.charAt(i);
-            int nAscii = (int)letter;
-            char newLetter = (char)(nAscii - n);
-            newString = newString + newLetter;
-        }
-        return newString;
+        return encrypt(s,-n);
     }
 }
