12471f97b4b8daebab5548188d12494a6437e24e
Augustin Gerasym <augustingerasym@gmail.com>
2023-06-04 18:38:25 +0300

    add task 1, lesson 11

---
+++ src/main/java/practical/lessonEleven/TaskOne.java
@@ -0,0 +1,11 @@
+package practical.lessonEleven;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        String input1 = "I love Java";
+        String input2 = "Java";
+        boolean isSubstring = input1.contains(input2);
+        System.out.println("Is a substring: " +  isSubstring);
+        
+    }
+}
