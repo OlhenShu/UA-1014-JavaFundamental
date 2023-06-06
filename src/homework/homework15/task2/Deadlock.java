b0acb0eca816981f9963ef5573fe00942789216c
Maryna <m51571750@gmail.com>
2023-06-06 21:41:27 +0300

    Added homework 15

--- src/homework/homework15/task2/Deadlock.java
+++ src/homework/homework15/task2/Deadlock.java
@@ -1,19 +1,27 @@
 package homework.homework15.task2;
 
+import homework.homework15.task3.Task3Thread;
+
 public class Deadlock {
-    public static void main() throws InterruptedException{
+      static public void main(String[] args){
         var threads = new Thread(new DeadThread());
-        threads.join();
-    }
-}
-class DeadThread implements Runnable {
-    @Override
-    public void run() {
-        var dl = new Deadlock();
+        threads.start();
         try {
-            dl.main();
+            threads.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        System.out.println("works");
+    }
+    static class DeadThread implements Runnable {
+        @Override
+        public void run() {
+            var dl = new Thread((Runnable) new Deadlock());
+            try {
+                dl.start();
+                dl.join();
+            } catch (ClassCastException | InterruptedException e) { }
+        }
     }
 }
+
