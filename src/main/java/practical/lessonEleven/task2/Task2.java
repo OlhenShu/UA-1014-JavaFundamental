0993085a4449dc6d7e9ef2db49b511fdd794f500
Augustin Gerasym <augustingerasym@gmail.com>
2023-06-08 17:44:26 +0300

    add task 2, lesson 11

--- src/main/java/practical/lessonEleven/task2/Task2.java
+++ src/main/java/practical/lessonEleven/task2/Task2.java
@@ -11,13 +11,13 @@ public class Task2 {
         String firstName = scanner.nextLine();
         System.out.print("Enter a middle name: ");
         String middleName = scanner.nextLine();
+        scanner.close();
 
         System.out.println(lastName + " " + firstName.charAt(0) + ". " + middleName.charAt(0) + ".");
         System.out.println(firstName);
         System.out.println(firstName + " " + middleName + " " + lastName);
     }
 }
-
 
 
 
